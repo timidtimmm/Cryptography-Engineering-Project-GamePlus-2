f2b4c9cba42103713044bcb5b127c74bb200628b
timidtimmm <142680672+timidtimmm@users.noreply.github.com>
2025-05-30 21:08:29 +0800

    Delete 2FA directory

--- 2FA/frontend/postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    // 直接用独立出来的 @tailwindcss/postcss
-    '@tailwindcss/postcss': {},
-    // 继续保留 Autoprefixer
-    autoprefixer: {},
-  },
-};
