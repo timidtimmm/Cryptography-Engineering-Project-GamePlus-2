4f5bed3b9e064ad43f5ac84791b9eb74de476177
timidtimmm <142680672+timidtimmm@users.noreply.github.com>
2025-05-30 21:09:55 +0800

    Delete 整合cry directory

--- 整合cry/backend/certs/ca_utils.py
+++
@@ -1,9 +0,0 @@
-from cryptography import x509
-from cryptography.hazmat.primitives import serialization
-
-def load_ca(ca_key_path: str, ca_cert_path: str):
-    with open(ca_key_path, "rb") as f:
-        ca_key = serialization.load_pem_private_key(f.read(), password=None)
-    with open(ca_cert_path, "rb") as f:
-        ca_cert = x509.load_pem_x509_certificate(f.read())
-    return ca_key, ca_cert
